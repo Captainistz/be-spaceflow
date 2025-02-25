f0ac0af68b0ff4dfdec22a8c495375e9b54838a8
Parit Siriphuwanich <phroggothesecond@gmail.com>
2025-02-25 12:49:25 +0700

    add new before mongoose.Schema({})

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
-const UserSchema = mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please add a name'],

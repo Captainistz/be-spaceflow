468e6d9a9d62c568b1201a5578ee449a29672394
neabk <aphipu23997@gmail.com>
2025-04-17 11:39:38 +0700

    add review voting

--- routes/review.js
+++ routes/review.js
@@ -1,21 +1,19 @@
 const express = require('express')
 const { protect, authorize } = require('../middleware/auth.js')
 
-const {
-  getReviews,
-  getReviewOfUser,
-  addReviews,
-  updateReview,
-  deleteReview,
-} = require('../controllers/reviews.js')
+const controller = require('../controllers/reviews.js')
 
 const router = express.Router({ mergeParams: true })
 
-router.get('/', getReviews)
-router.post('/', protect, authorize('user', 'admin'), addReviews)
+router.get('/', controller.getReviews)
+router.post('/', protect, authorize('user', 'admin'), controller.addReviews)
 
-router.get('/:user_id', protect, getReviewOfUser)
-router.put('/:review_id', protect, authorize('user', 'admin'), updateReview)
-router.delete('/:review_id', protect, authorize('user', 'admin'), deleteReview)
+router.get('/:user_id', protect, controller.getReviewOfUser)
+router.put('/:review_id', protect, authorize('user', 'admin'), controller.updateReview)
+router.delete('/:review_id', protect, authorize('user', 'admin'), controller.deleteReview)
+
+router.get('/:review_id/upvote', protect, controller.upvoteReview);
+router.get('/:review_id/downvote', protect, controller.downvoteReview);
+
 
 module.exports = router

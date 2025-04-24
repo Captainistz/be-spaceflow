9bb2b33829c11c4ab83d0442497c27f44adf73f2
Captainistz <captainistz@gmail.com>
2025-04-24 09:03:09 +0700

    ☁️ Update lock file and fix linting

--- routes/review.js
+++ routes/review.js
@@ -9,11 +9,20 @@ router.get('/', controller.getReviews)
 router.post('/', protect, authorize('user', 'admin'), controller.addReviews)
 
 router.get('/:user_id', protect, controller.getReviewOfUser)
-router.put('/:review_id', protect, authorize('user', 'admin'), controller.updateReview)
-router.delete('/:review_id', protect, authorize('user', 'admin'), controller.deleteReview)
+router.put(
+  '/:review_id',
+  protect,
+  authorize('user', 'admin'),
+  controller.updateReview,
+)
+router.delete(
+  '/:review_id',
+  protect,
+  authorize('user', 'admin'),
+  controller.deleteReview,
+)
 
-router.get('/:review_id/upvote', protect, controller.upvoteReview);
-router.get('/:review_id/downvote', protect, controller.downvoteReview);
-
+router.get('/:review_id/upvote', protect, controller.upvoteReview)
+router.get('/:review_id/downvote', protect, controller.downvoteReview)
 
 module.exports = router

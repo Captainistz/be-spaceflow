901531e27b5e71680d3ad9476d2364ffa205d954
Parit Siriphuwanich <phroggothesecond@gmail.com>
2025-04-25 19:13:37 +0700

    comment out space and status field

--- models/EventAttendance.js
+++ models/EventAttendance.js
@@ -11,16 +11,16 @@ const EventAttendanceSchema = new mongoose.Schema({
     ref: 'User',
     required: true,
   },
-  space: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'Space',
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ['upcoming', 'happening', 'ended'],
-    default: 'upcoming',
-  },
+  // space: {
+  //   type: mongoose.Schema.ObjectId,
+  //   ref: 'Space',
+  //   required: true,
+  // },
+  // status: {
+  //   type: String,
+  //   enum: ['upcoming', 'happening', 'ended'],
+  //   default: 'upcoming',
+  // },
   createdAt: {
     type: Date,
     default: Date.now,
